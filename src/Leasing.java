df1fa37ecdbca7db495ddc984fb3ae4d6b3b1749
Sefali21 <100235906+Sefali21@users.noreply.github.com>
2023-07-16 23:54:31 +0200

    Update Leasing.java

--- src/Leasing.java
+++ src/Leasing.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class Leasing {
+public class Leasing {
+    AutoAllgemeinInfo leasingYN=new AutoAllgemeinInfo();
+
+    private String leasingInfo;
+
+    public Leasing(String leasingInfo) {
+        this.leasingInfo = leasingInfo;
+    }
+
+    public String getLeasingInfo() {
+        return leasingInfo;
+    }
+
+    public void setLeasingInfo(String leasingInfo) {
+        this.leasingInfo = leasingInfo;
+    }
+
+    @Override
+    public String toString() {
+        if (leasingYN.isLeasing()){
+            return "Leasing{" +
+                    "leasingInfo='" + leasingInfo + '\'' +
+                    '}';
+        }
+        else return String.valueOf(leasingYN.isLeasing());
+    }
 }
